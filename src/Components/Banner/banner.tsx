428e13a970aa2d64012c49bf7c9c49dbd9fbff13
Mateusz Siniło <mateuszsinilo@gmail.com>
2023-02-06 16:08:14 +0100

    background image

--- src/Components/Banner/banner.tsx
+++ src/Components/Banner/banner.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Flex, Text, Image} from "@chakra-ui/react";
+import {Flex, Text, Box} from "@chakra-ui/react";
 
 export const Banner = () => {
     return (
         <Flex w='100%' position='relative' pointerEvents="none" userSelect="none">
-            <Image w='100%' h='100%' src='/assets/images/banner_placeholder.png'/>
+            <Box w='100%' h='454px' bgPosition='center' bgRepeat="no-repeat" backgroundImage='/assets/images/crop-gamer-hub.png' objectFit="cover"/>
             <Flex position='absolute' left='70px' top='40%' flexDir='column' gridGap='8px'>
                 <Text fontSize='42px' fontWeight='700'>Welcome to GamerHub</Text>
                 <Text fontSize='14px' w='70%'>Experience the future of gaming across multiple chains, all in  one place.</Text>
